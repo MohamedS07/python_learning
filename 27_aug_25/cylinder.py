d46d9fd3e08ab5589684c9ef6a90dcedfcc96bb9
Mohamed idhrish <mohamedidhrish215@gmail.com>
2025-10-08 14:59:59 +0530

    initial commit

---
+++ 27_aug_25/cylinder.py
@@ -0,0 +1,18 @@
+radius = 10
+height = 20
+pi = 3.14
+#volume
+v = pi * radius * radius * height
+print(v)
+#surface
+surface = 2 * pi * radius * (height * radius)
+print(surface)
+
+#sphere
+
+#volume 
+
+sphere = (4/3) * 3.14 * radius * radius * radius
+print(sphere)
+
+#test
