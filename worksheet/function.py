476dc527d933f3a48c10cd7d975acfb5736e4b60
Mohamed idhrish <mohamedidhrish215@gmail.com>
2025-10-08 14:35:24 +0530

    added new file

---
+++ worksheet/function.py
@@ -0,0 +1,19 @@
+# absolute
+a = -10
+print(abs(a))
+
+# power
+print(pow(2,3))
+
+# length
+b = "hello world"
+print(len(b))
+
+# function
+def voting_age (age):
+    if age >= 18:
+        print('ELIGIBLE')
+    else:
+        print('NOT ELIGIBLE')
+
+voting_age (18)
